5fa9a856c5101719a751a4de2506b3af46b59bac
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-09-28 23:21:15 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.use(rotas);
 
-const port = process.env.PORT || 3333;
+//const port = process.env.PORT || 3333;
 
 app.listen(process.env.PORT, () => {
     console.log(`Server ON. Escutando na porta ${process.env.PORT}.`)
