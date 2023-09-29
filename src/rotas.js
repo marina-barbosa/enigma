bcf5b93f5b616755de11e7cb4223f93f76bb236a
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-09-29 00:07:24 -0300

    adiciona html e css

--- src/rotas.js
+++ src/rotas.js
@@ -2,28 +2,154 @@ const express = require('express');
 const knex = require('./conexao.js');
 const rotas = express();
 
-// cadastro de usuario
+
 rotas.get('/', (req, res) => {
-    return res.status(200).json({ mensgame: '/escreva-a-resposta-aqui' });
+    const mensagemHTML = `
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #222;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        h1 {
+            color: #ffffff;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Acesse: https://wicked-foal-gabardine.cyclic.cloud/escreva-a-resposta-aqui</h1>    
+</body>
+</html>
+`;
+
+    return res.status(200).send(mensagemHTML);
 });
 
 rotas.get('/escreva-a-resposta-aqui', (req, res) => {
-    return res.status(200).json({ mensgame: 'Qual a cor do céu?' });
+    const mensagemHTML = `
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #222;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        h1 {
+            color: #fff;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Qual é a cor do céu?</h1>    
+</body>
+</html>
+`;
+    return res.status(200).send(mensagemHTML);
 });
 
 rotas.get('/azul', (req, res) => {
-    return res.status(200).json({ mensgame: 'O que está sempre à sua frente, mas você nunca vê?' });
+    const mensagemHTML = `
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #222;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        h1 {
+            color: #ffffff;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>O que está sempre à sua frente, mas você nunca vê?</h1>    
+</body>
+</html>
+`;
+    return res.status(200).send(mensagemHTML);
 });
 
 rotas.get('/futuro', (req, res) => {
-    return res.status(200).json({ mensgame: 'O que todos desejam ter, mas quando o têm, desejam compartilhar?' });
+    const mensagemHTML = `
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #222;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        h1 {
+            color: #ffffff;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>O que todos desejam ter, mas quando o têm, desejam compartilhar?</h1>    
+</body>
+</html>
+`;
+    return res.status(200).send(mensagemHTML);
 });
 
 rotas.get('/segredo', (req, res) => {
-    return res.status(200).json({ mensgame: 'Qual é o seu nome?' });
+    const mensagemHTML = `
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #222;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        h1,h2 {
+            color: #ffffff;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Qual é o seu nome?
+    <br>
+    <br>    
+    https://wicked-foal-gabardine.cyclic.cloud/nome/escreva-seu-nome-aqui
+    </h1> 
+     
+</body>
+</html>
+`;
+    return res.status(200).send(mensagemHTML);
 });
 
-rotas.get('/:nome', async (req, res) => {
+rotas.get('/nome/:nome', async (req, res) => {
     const nome = req.query;
     try {
         await knex('usuarios').insert({ nome: nome });
